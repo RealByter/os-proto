dfbadcfa54fe0c995e8d20da784dad88a660c545
RealByter <justavishay@gmail.com>
2023-08-19 13:40:36 +0300

    feat: add inb function to get byte from port

--- io.h
+++ io.h
@@ -8,5 +8,12 @@
  *  @param data The data to send to the I/O port
  */
 void outb(unsigned short port, unsigned char data);
+/** outb:
+ *  Returns a byte from the given I/O port. Defined in io.s
+ *
+ *  @param port The I/O port to send the data to
+ *  @return The byte from the port
+ */
+unsigned char inb(unsigned short port);
 
 #endif /* INCLUDE_IO_H */
